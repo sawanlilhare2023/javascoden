770f026bcc6011acc353a52c613b1bae9f355d8e
sawanlilhare2023 <sawanlilhare7@gmail.com>
2023-08-13 17:52:52 +0530

    My second commit

---
+++ DemTest.java
@@ -0,0 +1,20 @@
+
+class Mythread implements Runnable{
+    public void run(){
+        for(int i = 0;i<5;i++){
+
+            System.out.println("Child thread");
+        }
+    }
+}
+public class DemTest {
+    public static void main(String[] args) {
+        Mythread mt = new Mythread();
+        Thread t = new Thread(mt);
+        t.start();
+        for(int i = 0;i<5;i++){
+
+            System.out.println("main thread");
+        }
+    }
+}
--- HelloDemo.java
+++ HelloDemo.java
@@ -1,8 +1,9 @@
 public class HelloDemo {
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
 
+        System.out.println("Hello world");
+        System.out.println("Hello world2");
 
 
     }
